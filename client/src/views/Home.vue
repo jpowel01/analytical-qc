4da0e9737163b618eae42a231065c9cb1d7c6a00
AA\CRAMSLAN <ramsland.christian@epa.gov>
2023-01-26 03:24:27 -0500

    changes home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,56 +1,17 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col class="text-h3 text-left"> Batch Search </v-col>
-      
-      
+      <br>
+      <br>
+      <v-col class="text-h6 text-center"> Navigate to a list or record using the left menu. </v-col>      
     </v-row>
-          <p>To download an Excel Summary table of analytical data for a set of substances, input a list of DTXSIDs</p>
-    <div class="batch-search-controls">
-      <v-textarea outlined type="text" class="batch-search-input" rows="10" columns="25" v-model="search_box"></v-textarea>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      
-      <v-btn @click="batch_search()" class="batch-search-button">Search</v-btn>
-    </div>
   </v-container>
 </template>
-
-<style scoped>
-  .batch-search-input {
-    height: 150px;
-  }
-
-  .batch-search-button {
-    width: 150px;
-    margin: auto;
-    text-align: center;
-  }
-    .batch-search-controls {
-    height: 200px;
-    width: 200px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+<style>
 
 </style>
 
 <script>
 export default {
-      data() {
-      return {
-        search_box: ""
-      };
-      },
-
-      methods: {
-        async batch_search() {
-        }
-      }
-}
+  }
 </script>
